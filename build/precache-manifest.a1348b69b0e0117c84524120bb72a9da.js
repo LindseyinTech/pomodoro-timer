4b212d5bdbe68ff4c4b0d9d14c4ab0d33bd0f3e5
LindseyinTech <lindseyintech@gmail.com>
2021-12-22 15:00:34 -0800

    added readme

---
+++ build/precache-manifest.a1348b69b0e0117c84524120bb72a9da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4a9edf81114c755c9a70597dec28fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42149bf691ab67ebef12",
+    "url": "/static/css/main.109a8d76.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42149bf691ab67ebef12",
+    "url": "/static/js/main.2f4e63f2.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Pomodoro Timer 
+
+## Table of Contents
+* [Description](#description)
+* [Questions](#questions)
+
+## Description
+This project focused on utilizing React hooks to build a timer that implemented the Pomodoro method switching between timed work intervals and timed break intervals.
+![deployed](https://gyazo.com/f5a3729c0fabc84cba2534801ddc9a60.png)
+
+To view this project click [here](https://tomato-timer-gamma.vercel.app/)
+
+This project uses React and is hosted on Vercel.
+
+## Questions
+Questions? Reach out at [github.com/lindseyindev](github.com/lindseyindev)
